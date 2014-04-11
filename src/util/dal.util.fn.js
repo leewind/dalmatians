c3e1423887c843a8d6cb9cc132df4c3326bcbff8
Michael.Lee <leewind19841209@gmail.com>
2014-04-11 20:44:02 +0800

    [dev]删除零碎的文件，直接使用underscore

--- src/util/dal.util.fn.js
+++
@@ -1,8 +0,0 @@
-// @author yexiaochai
-// @see http://requirejs.org/docs/api.html
-// @see https://github.com/jashkenas/underscore
-// @description
-
-define(function(require, exports, modules) {
-
-});
