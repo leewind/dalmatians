968addffd14e7e16b5d5e8a8ccaf2531fc9516a3
l_wang <l_wang@ctrip.com>
2014-05-20 16:56:53 +0800

    更新app逻辑

--- src/app-manager.js
+++ src/app-manager.js
@@ -30,7 +30,8 @@ var Application = _.inherit({
     //view的根目录
     this.viewRootPath = 'app/views/';
 
-
+    this.animAPIs = {};
+    this.animatName = 'no';
 
   },
 
@@ -83,26 +84,135 @@ var Application = _.inherit({
 
     //首次不会触发路由监听，直接程序导入
     if (!this.isListeningRoute) {
-      this.onUrlChange(url);
+      this.loadViewByUrl(url);
     }
   },
 
-  onUrlChange: function () {
+  loadViewByUrl: function (url) {
+
+    //将url解析，解析出viewid，解析逻辑暂时忽略
+
+    var viewId = url;
+
+    this.switchView(viewId);
+
 
   },
 
-  //根据viewid，加载相关view
-  loadView: function (viewId, callback) {
-    requirejs([this._buildPath(viewId)], $.proxy(function (View) {
-      _.callmethod(callback, this, [View]);
-    }, this));
+  //根据viewId判断当前view是否实例化
+  viewExist: function (viewId) {
 
+    return false;
+  },
+
+
+  //根据viewid，加载view的类，并会实例化
+  //注意，这里只会返回一个view的实例，并不会显示或者怎样，也不会执行app的逻辑
+  loadView: function (viewId, callback) {
+
+    //每个键值还是在全局views保留一个存根，若是已经加载过便不予理睬
+    if (this.viewExist(viewId)) {
+      _.callmethod(callback, this, this.views.get(viewId));
+      return;
+    }
+
+    requirejs([this._buildPath(viewId)], $.proxy(function (View) {
+      var view = new View();
+
+      this.views.push(viewId, view);
+
+      //将当前view实例传入，执行回调
+      _.callmethod(callback, this, view);
+
+    }, this));
   },
 
   //根据viewId生成路径
   _buildPath: function (viewId) {
     return this.viewMapping[viewId] ? this.viewMapping[viewId] : this.viewRootPath + viewId;
+  },
+
+  //注意，此处的url可能是id，也可能是其它莫名其妙的，这里需要进行解析
+  forward: function (viewId) {
+
+    //解析viewId逻辑暂时省略
+    //......
+
+    this.switchView(viewId);
+
+  },
+
+  //view切换，传入要显示和隐藏的view实例
+  switchView: function (viewId) {
+    if (!viewId) return;
+
+    //    this.loadView();
+
+
+    this.loadView(viewId, function (view) {
+      this.lastView = this.curView;
+      this.curView = view;
+
+      //这里消去动画逻辑
+
+    });
+
+  },
+
+  //传入两个view的实例，这里会封装动画
+  startAnimation: function (inView, outView) {
+    if (!inView) return;
+
+    //若是存在outView就需要动画，否则直接显示即可
+
+    if (!outView) { inView.show(); this._onSwitchEnd(inView);  return; }
+
+    var switchFn = this.animAPIs[this.animatName];
+
+    if (switchFn) {
+      switchFn(inView, outView, $.proxy(function (inView, outView) {
+        this._onSwitchEnd(inView, outView);
+      }, this));
+    } else {
+      this._onSwitchEnd(inView, outView);
+    }
+
+  },
+
+  //统一处理切换结束的逻辑
+  _onSwitchEnd: function (inView, outView) {
+    outView.hide();
+    inView.show();
+
+  },
+
+
+  //根据viewId显示一个view
+  showView: function (id, callback) {
+    if (!id) return;
+    this.loadView(id, function (view) {
+
+      //view自动会触发各个关于show的事件
+      view.show();
+
+    });
+
+  },
+
+  //根据viewId隐藏一个view
+  hideView: function (id, callback) {
+    if (!id) return;
+
+    this.loadView(id, function (view) {
+
+      //view自动会触发各个关于show的事件
+      view.hide();
+
+    });
+
   }
+
+
 
 
 
